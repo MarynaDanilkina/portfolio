498481e671fdee1a3caedbbe0ae511a708ae9637
Maryna <danilkinamari99@gmail.com>
2024-10-07 10:34:44 +0300

    fix: fix

--- react/data/experience.ts
+++ react/data/experience.ts
@@ -15,7 +15,7 @@ Stack: React, react-dnd, react-hook-form, react-i18next, react-redux, sass)`,
     id: 1,
     title: "Frontend developer",
     company: "SagTech",
-    date: "January 2023 - March 2024",
+    date: "January 2023 - Present",
     text: `Worked on two projects: B2T and SagTech website
 B2T (stack: Next.js, TypeScript, Tailwind, Redux Toolkit, Swiper.js)<br/>
 - Developed the client part of the application from scratch.<br/>
@@ -34,15 +34,5 @@ apexcharts, swiper, zod)<br/>
 - Used framer-motion to dynamically control animations, improving visual effects.<br/>
 - Integrated ApexCharts for visual data visualization.<br/>
 - Optimized data loading by 80% using GraphQL.`,
-  },
-  {
-    id: 2,
-    title: "Frontend developer",
-    company: "WebLab",
-    date: "March 2024 - Present",
-    text: `- Developed web applications using React with Redux Toolkit, TypeScript, Tailwind CSS, and Zustand, from prototype to finished product with a responsive interface.<br/>
-- Integrated external interfaces with internal services, followed REST API principles, and actively collaborated with the development team, from designers to QA engineers.<br/>
-- Created an authorization system and a user dashboard with an interface for managing personal data and settings. This improved the user experience, simplified access to key features, and enhanced interaction with internal systems.
-`,
-  },
+  }
 ]
